5bf90206b831cea2c66318e1d08c23bb7dadbd38
Durham Region <ontario.durham.region@gmail.com>
2025-07-08 16:40:41 -0400

    fix: weblink truncation

--- src/components/EntryCards/EntryCards.jsx
+++ src/components/EntryCards/EntryCards.jsx
@@ -5,7 +5,7 @@ const InfoField = ({ label, children }) => {
   if (!children) {
     return null
   }
-  return <div>
+  return <div class="flex">
     <span class="font-medium text-gray-600 inline-block w-20">{label}:</span>
     {children}
   </div>
@@ -16,12 +16,11 @@ const Weblink = ({ url = '' }) => {
     .replace('https://', '')
     .replace('www.', '')
     .substring(0, 60)
-  const formatedUrl = shortUrl.length > 59 ? shortUrl.concat('...') : shortUrl
 
   return <a
-    class="p-0 truncate w-[30em] overflow-ellipsis"
+    class="inline-block max-w-xs truncate"
     href={url}>
-    {formatedUrl}
+    {shortUrl}
   </a>
 }
 
@@ -29,7 +28,6 @@ const Card = (entry) => (
   <div class="p-3 hover:bg-white border border-gray-300 rounded-lg shadow">
     <div><span class="font-medium text-lg text-black">{entry.name}</span></div>
     <div class="my-2"><span class="font-medium text-sm text-gray-700">{entry.description}</span></div>
-    {/* {InfoField('Phone', entry.phone)} */}
     <InfoField label='Phone'>{entry.phone}</InfoField>
     <InfoField label='Website'>
       <Weblink url={entry.website} />
