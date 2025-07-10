a12e4effc8a83cab8e577831abe28b16229be2bc
Durham Region <ontario.durham.region@gmail.com>
2025-07-10 14:17:15 -0400

    chore: header as h1 for better sso

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ export const Layout = () => (
   <div class="flex flex-col h-screen w-full">
     <div class="flex h-14 px-1 justify-center bg-emerald-800">
       <div class="flex h-14 justify-center items-center w-full max-w-4xl text-white font-bold">
-        <span class="inline-block text-2xl">Pickering Toddler Guide</span>
+        <h1 class="text-2xl">Pickering Toddler Guide</h1>
       </div>
     </div>
 
