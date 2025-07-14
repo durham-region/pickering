c0f5c828daed449d976f286d0005339225253d8d
Rafael Santos <rafacandev@gmail.com>
2025-07-14 17:00:30 -0400

    chore: configure DaisyUI

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(), solidPlugin()],
+  plugins: [solidPlugin(), tailwindcss()],
   base: '/pickering/',
   server: {
     port: 3000,
   },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['node_modules/@testing-library/jest-dom/vitest'],
+    // if you have few tests, try commenting this
+    // out to improve performance:
+    isolate: false,
+  },
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    conditions: ['development', 'browser'],
   },
 });
