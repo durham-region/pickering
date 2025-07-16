a75392e6f14d9dcdd5992a479cb1d294a925315c
Durham Region <ontario.durham.region@gmail.com>
2025-07-15 22:16:11 -0400

    feat: open/close category filters

--- src/components/EntriesPage/CategoryFilter.jsx
+++ src/components/EntriesPage/CategoryFilter.jsx
@@ -1,3 +1,4 @@
+import { createSignal } from "solid-js"
 import { Adjustment } from "../../icons/Adjustment"
 import { categories } from "../EntryCards/entries"
 
@@ -19,32 +20,40 @@ export const CategoryFilter = (props) => {
   /** @type {HTMLFormElement} */
   let formRef
 
+  const [isOpen, setOpen] = createSignal(false)
+
   const handleOnClick = () => {
     const selected = Array
       .from(formRef.querySelectorAll('input:checked'))
       .map(input => input.value)
+
     props.onChange(selected)
+    setOpen(false)
   }
 
   return (
-    <div class="bg-base-100 border-base-300 collapse border collapse-arrow collapse-open">
-      <input type="checkbox" class="peer" />
-      <div class="collapse-title bg-base-100 flex gap-x-3">
+    <div
+      class={`bg-base-100 border-base-300 collapse border collapse-arrow 
+              ${isOpen() ? 'collapse-open' : ''}`}>
+                
+      <div class="collapse-title bg-base-100 flex gap-x-3 cursor-pointer" onClick={() => setOpen(o => !o)}>
         <Adjustment /> Filter categories
       </div>
+
       <div class="collapse-content">
         <form class="grid md:grid-cols-2 lg:grid-cols-3" ref={formRef}>
           <Filters />
         </form>
-      </div>
-      <div class="flex justify-center">
-        <button
-          class="btn btn-primary px-8 mb-4"
-          type="button"
-          onClick={handleOnClick}
-        >
-          Filter
-        </button>
+
+        <div class="flex justify-center">
+          <button
+            class="btn btn-primary px-8 mb-4"
+            type="button"
+            onClick={handleOnClick}
+          >
+            Filter
+          </button>
+        </div>
       </div>
     </div>
   )
