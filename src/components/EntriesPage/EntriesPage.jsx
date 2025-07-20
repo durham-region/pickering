3ca91d554c5a2d71e045d962dacb43de7fb83a72
Durham Region <ontario.durham.region@gmail.com>
2025-07-20 19:23:49 -0400

    feat: add qr-code dialog

--- src/components/EntriesPage/EntriesPage.jsx
+++ src/components/EntriesPage/EntriesPage.jsx
@@ -1,10 +1,12 @@
 import { EntryCards } from "../EntryCards/EntryCards"
 import pdfUrl from "../../assets/pickering-toddler-guide.pdf"
 import { Printer } from "../../icons/Printer"
+import { QrCode } from "../../icons/QrCode"
 import { DownloadFile } from "../../icons/DownloadFile"
 import { CategoryFilter } from "./CategoryFilter"
 import { createSignal } from "solid-js"
 import { categories } from "../EntryCards/entries"
+import { createQrModal } from "./QrModel"
 
 const Icon = ({ footer = '', onClick = () => null, children }) =>
   <div class="flex flex-col items-center bg-white py-3 px-8 md:px-16 border rounded-md border-emerald-900 cursor-pointer hover:bg-emerald-50 hover:shadow-md"
@@ -17,6 +19,7 @@ const Icon = ({ footer = '', onClick = () => null, children }) =>
 
 export const EntriesPage = () => {
   const [filters, setFilters] = createSignal(Object.keys(categories))
+  const [qrModal, showModal] = createQrModal()
   
   return <div class="space-y-2 my-5">
     <article class="prose max-w-none">
@@ -27,9 +30,12 @@ export const EntriesPage = () => {
     <div class="mt-8 mb-6 flex space-x-5 justify-center">
       <div class="flex gap-2">
         <Icon footer="Print" onClick={() => window.print()}><Printer /></Icon>
-        <Icon footer="PDF" onClick={() => window.open(pdfUrl, '_blank')}><DownloadFile /></Icon>
+        <Icon footer="pdf" onClick={() => window.open(pdfUrl, '_blank')}><DownloadFile /></Icon>
+        <Icon footer="qr" onClick={() => showModal()}><QrCode /></Icon>
       </div>
     </div>
+
+    {qrModal}
 
     <CategoryFilter onChange={setFilters}/>
 
---
+++ src/components/EntriesPage/QrModel.jsx
@@ -0,0 +1,23 @@
+import qrCodeUrl from "../../assets/qr-code.svg"
+
+
+export const createQrModal = () => {
+  let modalRef;
+
+  const showModal = () => modalRef.showModal()
+
+  const view =
+    <dialog ref={el => modalRef = el} class="modal">
+      <div class="modal-box flex flex-col items-center">
+        <img src={qrCodeUrl} class="w-full"/>
+        <p class="py-4 text-sm">https://durham-region.github.io/pickering</p>
+        <div class="modal-action">
+          <form method="dialog">
+            <button class="btn btn-primary">Close</button>
+          </form>
+        </div>
+      </div>
+    </dialog>
+
+  return [view, showModal]
+}
